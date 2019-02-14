eb9878ef513ca14478ac707e4e14c418b307b1da
iSayme <isaymeorg@gmail.com>
2019-02-14 20:44:31 +0800

    support aes-192-cfb

--- shadowsocks/cipher/methods.go
+++ shadowsocks/cipher/methods.go
@@ -2,6 +2,7 @@ package cipher
 
 var cipherMethods = map[string]*cipherInfo{
 	"aes-128-cfb":   &cipherInfo{16, 16, newAESCFBEncryptStream, newAESCFBDecryptStream},
+	"aes-192-cfb":   &cipherInfo{24, 16, newAESCFBEncryptStream, newAESCFBDecryptStream},
 	"aes-256-cfb":   &cipherInfo{32, 16, newAESCFBEncryptStream, newAESCFBDecryptStream},
 	"rc4-md5":       &cipherInfo{16, 16, newRC4MD5Stream, newRC4MD5Stream},
 	"chacha20":      &cipherInfo{32, 8, newChaCha20Stream, newChaCha20Stream},
