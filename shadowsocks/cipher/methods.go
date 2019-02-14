49e071d7b1754c9c2f61c9e3aef211b1a72cc3f6
iSayme <isaymeorg@gmail.com>
2019-02-14 20:53:32 +0800

    support des-cfb

--- shadowsocks/cipher/methods.go
+++ shadowsocks/cipher/methods.go
@@ -7,6 +7,7 @@ var cipherMethods = map[string]*cipherInfo{
 	"aes-128-ctr":   &cipherInfo{16, 16, newAESCTRStream, newAESCTRStream},
 	"aes-192-ctr":   &cipherInfo{24, 16, newAESCTRStream, newAESCTRStream},
 	"aes-256-ctr":   &cipherInfo{32, 16, newAESCTRStream, newAESCTRStream},
+	"des-cfb":       &cipherInfo{8, 8, newDESCFBEncryptStream, newDESCFBDecryptStream},
 	"rc4-md5":       &cipherInfo{16, 16, newRC4MD5Stream, newRC4MD5Stream},
 	"chacha20":      &cipherInfo{32, 8, newChaCha20Stream, newChaCha20Stream},
 	"chacha20-ietf": &cipherInfo{32, 12, newChaCha20Stream, newChaCha20Stream},
