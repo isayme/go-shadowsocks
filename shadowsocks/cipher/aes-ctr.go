ccdcf7c2fd598bbe561f639c97e2600d88890b29
iSayme <isaymeorg@gmail.com>
2019-02-14 20:47:33 +0800

    support aes-128-ctr, aes-192-ctr, aes-256-ctr

---
+++ shadowsocks/cipher/aes-ctr.go
@@ -0,0 +1,15 @@
+package cipher
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+)
+
+func newAESCTRStream(key, iv []byte) (cipher.Stream, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	return cipher.NewCTR(block, iv), nil
+}
