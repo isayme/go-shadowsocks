67799499e31a058523660066ab25ee65970d1fed
iSayme <isaymeorg@gmail.com>
2019-02-14 20:43:27 +0800

    support chacha20, chacha20-itef, cast5-cfb, bf-cfb

--- go.mod
+++ go.mod
@@ -1,7 +1,10 @@
 module github.com/isayme/go-shadowsocks
 
 require (
+	github.com/aead/chacha20 v0.0.0-20180709150244-8b13a72661da
 	github.com/panjf2000/ants v0.0.0-20181002135227-29730bb70343
 	github.com/pkg/errors v0.8.0
 	github.com/rs/zerolog v1.9.1
+	golang.org/x/crypto v0.0.0-20190211182817-74369b46fc67
+	golang.org/x/sys v0.0.0-20190213121743-983097b1a8a3 // indirect
 )
--- shadowsocks/cipher/methods.go
+++ shadowsocks/cipher/methods.go
@@ -1,8 +1,11 @@
 package cipher
 
 var cipherMethods = map[string]*cipherInfo{
-	"aes-128-cfb": &cipherInfo{16, 16, newAESCFBEncryptStream, newAESCFBDecryptStream},
-	"aes-256-cfb": &cipherInfo{32, 16, newAESCFBEncryptStream, newAESCFBDecryptStream},
-
-	"rc4-md5": &cipherInfo{16, 16, newRC4MD5Stream, newRC4MD5Stream},
+	"aes-128-cfb":   &cipherInfo{16, 16, newAESCFBEncryptStream, newAESCFBDecryptStream},
+	"aes-256-cfb":   &cipherInfo{32, 16, newAESCFBEncryptStream, newAESCFBDecryptStream},
+	"rc4-md5":       &cipherInfo{16, 16, newRC4MD5Stream, newRC4MD5Stream},
+	"chacha20":      &cipherInfo{32, 8, newChaCha20Stream, newChaCha20Stream},
+	"chacha20-ietf": &cipherInfo{32, 12, newChaCha20Stream, newChaCha20Stream},
+	"cast5-cfb":     &cipherInfo{16, 8, newCast5EncryptStream, newCast5DecryptStream},
+	"bf-cfb":        &cipherInfo{16, 8, newBlowfishEncryptStream, newBlowfishDecryptStream},
 }
