3502c11ff6e7a1f121462ee74ece857f9df9dc7b
iSayme <isaymeorg@gmail.com>
2019-02-14 21:32:33 +0800

    support rc4-md5-6

--- shadowsocks/cipher/methods.go
+++ shadowsocks/cipher/methods.go
@@ -9,6 +9,7 @@ var cipherMethods = map[string]*cipherInfo{
 	"aes-256-ctr":   &cipherInfo{32, 16, newAESCTRStream, newAESCTRStream},
 	"des-cfb":       &cipherInfo{8, 8, newDESCFBEncryptStream, newDESCFBDecryptStream},
 	"rc4-md5":       &cipherInfo{16, 16, newRC4MD5Stream, newRC4MD5Stream},
+	"rc4-md5-6":     &cipherInfo{16, 6, newRC4MD5Stream, newRC4MD5Stream},
 	"chacha20":      &cipherInfo{32, 8, newChaCha20Stream, newChaCha20Stream},
 	"chacha20-ietf": &cipherInfo{32, 12, newChaCha20Stream, newChaCha20Stream},
 	"cast5-cfb":     &cipherInfo{16, 8, newCast5EncryptStream, newCast5DecryptStream},
