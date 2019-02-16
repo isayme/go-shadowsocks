e0ce740659e1e13142f9087219f7848562f01ec4
iSayme <isaymeorg@gmail.com>
2019-02-16 09:48:03 +0800

    update md5

--- shadowsocks/cipher/rc4-md5.go
+++ shadowsocks/cipher/rc4-md5.go
@@ -2,15 +2,13 @@ package cipher
 
 import (
 	"crypto/cipher"
-	"crypto/md5"
 	"crypto/rc4"
+
+	"github.com/isayme/go-shadowsocks/shadowsocks/util"
 )
 
 func newRC4MD5Stream(key, iv []byte) (cipher.Stream, error) {
-	h := md5.New()
-	h.Write(key)
-	h.Write(iv)
-	rc4key := h.Sum(nil)
+	rc4key := util.MD5(key, iv)
 
 	return rc4.NewCipher(rc4key)
 }
